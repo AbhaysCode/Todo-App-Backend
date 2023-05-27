7412035b0a4bf9f46ac34d0dcb437f7d4fe150e4
AbhaysCode <111600737+AbhaysCode@users.noreply.github.com>
2023-05-27 10:46:42 +0530

    CRUD DONE

--- controllers/todo.js
+++ controllers/todo.js
@@ -1,7 +1,7 @@
 const Todo = require("../models/todo");
 const mongoose = require("mongoose");
 
-function create(req,res,next){
+const create = (req,res,next) =>{ 
     let id = req.body.id;
     let category = req.body.category;
     let date = req.body.date;
@@ -20,5 +20,30 @@ function create(req,res,next){
         res.send(data);
     })
 }
-
+const view = (req,res,next) =>{
+    Todo.find({}).then((data)=>{
+        res.send(data)
+      })
+}
+const update = (req,res,next) =>{
+    Todo.findByIdAndUpdate(req.params.id,req.body).then(updatedTodo => {
+        res.json(updatedTodo);
+      })
+      .catch(error => {
+        console.error(error);
+        res.status(500).json({ message: 'An error occurred in updating Todo' });
+      });
+}
+const remove = (req,res,next) =>{
+    Todo.findByIdAndDelete(req.params.id).then(deletedTodo => {
+        res.json(deletedTodo);
+      })
+      .catch(error => {
+        console.error(error);
+        res.status(500).json({ message: 'An error occurred in deleting Todo' });
+      });
+}
 module.exports.create = create;
+module.exports.view =  view;
+module.exports.update =  update;
+module.exports.remove =  remove;
--- routes/todo.js
+++ routes/todo.js
@@ -1,11 +1,14 @@
 const express = require("express");
 const router = express();
 const create = require("../controllers/todo");
+const view = require("../controllers/todo");
+const update = require("../controllers/todo");
+const remove = require("../controllers/todo");
 const bodyparser = require("body-parser");
 
 router.use(bodyparser.json());
 router.post('/create', create.create);
-router.get('/testing',(req,res)=>{
-    res.send("Testing Successs");
-})
+router.get('/', view.view);
+router.patch('/:id', update.update);
+router.delete('/delete/:id', remove.remove);
 module.exports =  router;
